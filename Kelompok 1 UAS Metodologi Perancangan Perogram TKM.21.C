05319375c24798b0fa14652d84ffb88484e90078
Nabilaoktaviani <117067003+Nabilaoktaviani@users.noreply.github.com>
2024-01-24 15:13:51 +0700

    Update and rename README.md to Kelompok 1 UAS Metodologi Perancangan Perogram TKM.21.C
    Nama Kelompok 1 TKM.21.C :
    - Kurnianti Santiawan
    - Nabila Oktaviani
    - Neng Lisna Dania Lestari
    - Silvi Dwi Lestari
    link youtube :
    https://youtu.be/ELyRyp_vDAY?si=WHZsolNpwO9m66S3

---
+++ Kelompok 1 UAS Metodologi Perancangan Perogram TKM.21.C
@@ -0,0 +1,7 @@
+Nama Kelompok 1 TKM.21.C :
+- Kurnianti Santiawan
+- Nabila Oktaviani
+- Neng Lisna Dania Lestari
+- Silvi Dwi Lestari
+link youtube :
+https://youtu.be/ELyRyp_vDAY?si=WHZsolNpwO9m66S3
